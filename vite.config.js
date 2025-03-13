6e568edd506aaaa988f7f59db234f6fcccf82f10
GoddyS <goddys777@icloud.com>
2025-03-14 01:18:00 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: true,
-    allowedHosts: ['gameofbotania.fun'],  // Добавьте ваш домен сюда
-    cors: true,  // Включите CORS, если требуется
+    allowedHosts: ['gameofbotania.fun'],  // Ваш домен
+    cors: true,  // Включите CORS, если необходимо
+    hmr: {
+      protocol: 'wss',  // Убедитесь, что используете безопасный протокол
+      host: 'gameofbotania.fun',  // Указываем домен
+      port: 5173,  // Убедитесь, что порт совпадает с портом вашего локального сервера
+    },
   },
 });
