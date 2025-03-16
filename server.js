c6fd75bea1be7c227ba97522b923061b8340249d
GoddyS <goddys777@icloud.com>
2025-03-16 23:17:45 +0300

    fix

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-const PORT = 3000;
+const PORT = 3001;
 
 // Пример данных соседей
 const neighbors = {
@@ -12,19 +8,12 @@ const neighbors = {
     '2': { name: 'Сосед 2', plants: [{ type: 'Тюльпан', stage: 5 }] }
 };
 
-io.on('connection', (socket) => {
-    socket.on('disconnect', () => {});
-    socket.on('updatePlant', (data) => {
-        io.emit('plantUpdated', data);
-    });
-});
-
 app.get('/neighbor/:id', (req, res) => {
     const neighborId = req.params.id;
     const neighborData = neighbors[neighborId] || { error: 'Сосед не найден' };
     res.json(neighborData);
 });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
