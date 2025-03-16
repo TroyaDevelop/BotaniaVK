507e3a7e4f935d0943bbe7b0d8bb4a3336367fde
GoddyS <goddys777@icloud.com>
2025-03-16 23:44:55 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: 5174, // Явно укажите порт
+    port: 5173, // Явно укажите порт
     hmr: {
       host: 'localhost',
       protocol: 'ws', // Только ws для разработки
