4591afdc51b406c316efa9802c5d07e1eacc0314
GoddyS <goddys777@icloud.com>
2025-03-18 01:57:08 +0300

    try fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,21 @@
 import '../style.css';
-import bridge from '@vkontakte/vk-bridge';
+import VK from '@vkontakte/vk-bridge';
 import Phaser from 'phaser';
 import MainScene from '../scenes/MainScene.js';
 
+const app_id = 53221746;  // get the app_id from .env file
+
+// initialize VK Bridge with the obtained id
+VK.init({apiId: parseInt(app_id)});
+
 // Initialize VK API
-bridge.send('VKWebAppInit');
+VK.send('VKWebAppInit');
 
 const config = {
     type: Phaser.AUTO,
     width: 1200,
     height: 800,
-    scene: new MainScene({ bridge })
+    scene: new MainScene()
 };
 
 const game = new Phaser.Game(config);
-//
