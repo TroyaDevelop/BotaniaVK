f230d435fb7a49c399d0344754147a811b967494
GoddyS <goddys777@icloud.com>
2025-03-18 18:44:04 +0300

    zaebalo

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-const VK = require('@vkontakte/vk-bridge');
-VK.init({ appId: YOUR_APP_ID });
-// Использование методов VK...
+import bridge from '@vkontakte/vk-bridge';
+
+// Инициализация VK Bridge
+bridge.send('VKWebAppInit')
+    .then(() => {
+        console.log('VK Bridge initialized');
+    })
+    .catch((error) => {
+        console.error('VK Bridge initialization failed', error);
+    });
 
 async function testVkBridge() {
     try {
-      const userInfo = await VK.call('VKWebAppGetUserInfo');
-      console.log(userInfo); // В консоли должно появится информация о пользователе
+        const userInfo = await bridge.send('VKWebAppGetUserInfo');
+        console.log('User Info:', userInfo);
     } catch (error) {
-      console.error("Error:", error);
+        console.error("Error:", error);
     }
-  }
-  testVkBridge();
+}
+
+testVkBridge();
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: './main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true, // Очистка папки dist перед каждой сборкой
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime']
+                    },
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: 'index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'style.css', to: 'style.css' },
+            ],
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+        open: true, // Автоматически открывать браузер при запуске сервера
+    },
+    mode: 'development',
+};
